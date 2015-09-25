9d5b03df9bc5e4cc96582ca68305e72b05286cd0
Nikita Bogachov <nbogach@hotmail.com>
2015-09-25 21:08:48 +0300

    My ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
 ## ProgrammingAssignment2
 ## functions do
 
-## Write a short comment describing this function
+## Function makeCacheMatrix is similar to makeCacheVector shown in example
 
 makeCacheMatrix <- function(x = matrix()) {
-
+            
+            ## Make stored Soved Matrix NULL 
+            solved_m <- NULL 
+            
+            ## Store Matrix passed to function as parameter
+            set <- function(y) {
+                  x <<- y
+                  sovled_m <<- NULL
+            }
+            
+            ## Get stored Matrix
+            get <- function() x
+            
+            ## Store calculated solved Matrix in "cache"
+            setsolved_m <- function(solved) solved_m <<- solved
+            
+            ## Get solved Matrix from "cache"
+            getsolved_m <- function() solved_m
+            
+            ## Return functions described above to object assigned by makeCacheMatrix
+            list(set = set, get = get,
+                 setsolved_m = setsolved_m,
+                 getsolved_m = getsolved_m)
 }
 
 
-## Write a short comment describing this function
+## This function returns solved matrix from cache if any. Otherwise calculate solved matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+      ## Get solved Matrix from cache
+      solved_m <- x$getsolved_m()
+      
+      ## Check if data exists, i.e. is not NULL
+      if(!is.null(solved_m)) {
+            message("getting cached data")
+            return(solved_m)
+      }
+      
+      ## No solved Matrix in cache. Start calculation...
+      data <- x$get()
+      solved_m <- solve(data, ...)
+      x$setsolved_m(solved_m)
+      solved_m
+
 }
+
